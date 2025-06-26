ea77b1e2d29d2ade114b0eeaa6e8b44ffb459100
nevil811 <nevilbarick69@gmail.com>
2025-06-27 01:21:28 +0530

    Functions and Lambda functions

---
+++ Functions and Modules/basics.py
@@ -0,0 +1,13 @@
+def add(num1,num2):
+  return num1+num2
+
+print(add(1,2))
+
+def greet(name,age):
+  return f"You are Welcome Mr.{name} and you are now {age} years old"
+print(greet("Nevil",18))
+
+def avg(a,b,c):
+  s= (a+b+c)/3
+  print(s)
+avg(1,2,3)

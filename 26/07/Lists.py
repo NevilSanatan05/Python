de9b9cff96784f79609b5a69ecd7f2a0c414e024
nevil811 <nevilbarick69@gmail.com>
2025-07-26 12:05:13 +0530

    lists and tuples

---
+++ 26/07/Lists.py
@@ -0,0 +1,31 @@
+#Lists in Python
+# l1 = [2,5,1,7,3,9]
+# print(l1)
+# l1.sort()
+# print(l1)
+# l1.reverse()
+# print(l1)
+# l1.append(4)
+# print(l1)
+# l1.insert(2, 6)
+# print(l1)
+# l1.remove(5)
+# print(l1)
+
+# fruits =[]
+# for i in range(5):
+#     fruit = input(f"Enter a fruit {i+1}: ")
+#     fruits.append(fruit)
+# print(fruits)
+# print(type(fruits))
+
+# marks = []
+# for i in range(6):
+#     mark = int(input(f"Enter mark {i+1}: "))
+#     marks.append(mark)
+# print(marks)
+# print(type(marks))
+
+l2 = [1, 2, 3, 4, 5]
+total = sum(l2)
+print(f"Total of l2: {total}")
---
+++ 26/07/Tuples.py
@@ -0,0 +1,7 @@
+#Tuples are immutable sequences in Python, used to store multiple items in a single variable.
+a = (2, 5, 1, 7, 3, 9)
+print(a.count(2))
+print(a.index(7))
+
+b = (7, 0, 8, 0, 0, 9)
+print(b.count(0))

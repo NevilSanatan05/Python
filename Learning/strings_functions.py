3aaf64760042ac2c92f353f6334d070cb84c552c
nevil811 <nevilbarick69@gmail.com>
2025-08-02 21:34:30 +0530

    strings and its functions

---
+++ Learning/strings_functions.py
@@ -0,0 +1,28 @@
+name = "Nevil Sanatan"
+'''print(name)
+print(name[0])
+print(name[0:])
+print(name[:5])
+print(name[-4:-1])
+print(name[-3:])
+print(name[::-1])
+print(name.upper())
+print(name.lower())
+print(name.capitalize())
+print(name.swapcase())
+print(len(name))'''
+
+'''s = "Python123"
+print(s.isalpha())
+print(" ".isspace())
+
+m = "I like Python Programming"
+print(m.find("like"))
+print(m.replace("Python","Java"))'''
+
+f = "apple,banana,cherry"
+print(f.split(","))
+
+words = ["apple","banana","cherry"]
+print(" ".join(words))
+

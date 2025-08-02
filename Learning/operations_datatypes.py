9c6e1403cabf2381844b07796ecf9ffd86797c68
nevil811 <nevilbarick69@gmail.com>
2025-08-02 21:01:48 +0530

    operators and conditions

---
+++ Learning/operations_datatypes.py
@@ -0,0 +1,21 @@
+x = 2
+y=4
+#print(x==2 and x==y)
+#print(x==2 or y==5)
+#print(not(x!=2))
+
+a = 10
+a += 5
+a -= 10
+'''print(a)
+print(a)'''
+
+age = int(input("Enter your age : "))
+if age>=18 and age<=100:
+  print("You can vote")
+elif age>=10 and age<18:
+  print("You can't vote because you are about to get mature")
+else:
+  print("You are a bacha man")
+
+

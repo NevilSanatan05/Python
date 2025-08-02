32ce70eee89d5c6a888fbc2eac0e3fbf7c4887eb
nevil811 <nevilbarick69@gmail.com>
2025-08-02 20:48:53 +0530

    data_types

---
+++ Learning/data_types.py
@@ -0,0 +1,29 @@
+#Lists
+fruits = ["Apple","Banana","Mango"]
+print(fruits)
+
+#Tuples
+animals = (10,20)
+print(animals)
+
+#sets
+numbers = {1,2,3}
+print(numbers)
+
+#dictionary
+student={
+  "name":"Nevil",
+  "age":20
+}
+print(student)
+
+
+name = "Nevil"
+age = int(input("Enter your age : "))
+print("My name is  : ",name ," and my age is " ,age)
+print(f"My name is {name} and my age is {age}")
+
+# Taking input and displaying info
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+print(f"Hello {name}! You are {age} years old.")

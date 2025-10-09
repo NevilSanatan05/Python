34577efa84fcb3362e2350970159518ceafeda6f
nevil811 <nevilbarick69@gmail.com>
2025-10-09 12:01:45 +0530

    Data types , Typecasting , User Input , Escape Sequence

---
+++ NEW/fundamentals.py
@@ -0,0 +1,52 @@
+#Data Types in Python
+# a = 68
+# print(type(a))
+
+# b = 5.5
+# print(type(b))
+
+# c = 1+2j
+# print(type(c))
+
+# d = "Nevil"
+# print(type(d))
+
+# e = True
+# print(type(e))
+
+#TypeCasting
+# m = "45"
+# print(type(m))
+# g= int(m)
+# print(type(g))
+
+
+# k = 45
+# print(type(k))
+# n = str(k)
+# print(type(n))
+
+#User Input
+# name = str(input("Enter your name : "))
+# age = int(input("Enter your age : "))
+# print(f"My name is {name} and my age is {age}")
+
+# a = int(input("Enter a number : "))
+# b = int(input("Enter another number : "))
+# print("The sum is : ", a+b)
+
+#Comments in Python
+# This is a single line comment 
+'''
+This is a multiple line comment
+You can write anything here
+'''
+#Escape Sequences
+# print("My name is Nevil \nI am 19 years old")
+# print("We all shouls be\tgood people")
+# print("This is backslash\\")
+# print("This is double quote\"")
+# print('This is single quote\'')
+# print("Hello world","Raj",sep="//")
+
+#Operators in Python

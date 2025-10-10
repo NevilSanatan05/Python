14a14d970448ab0cbd033feefacf0db24ef83b22
nevil811 <nevilbarick69@gmail.com>
2025-10-10 11:35:10 +0530

    fundamentals.py

--- NEW/fundamentals.py
+++ NEW/fundamentals.py
@@ -50,3 +50,47 @@ You can write anything here
 # print("Hello world","Raj",sep="//")
 
 #Operators in Python
+# a=int(input("Enter a number : ") )
+# b=int(input("Enter another number : ") )
+# print("The sum is : ",a+b)
+# print("The difference is : ",a-b)
+# print("The product is : ",a*b)
+# print("The division is : ",a/b)
+# print("The floor division is : ",a//b)
+# print("The modulus is : ",a%b)
+# print("The exponent is : ",a**b)
+# print("The average is : ",(a+b)/2)
+# print("The maximum is : ",max(a,b))
+# print("The minimum is : ",min(a,b))
+# print(10==5)
+# print(10!=5)
+# print(10>5)
+# print(10<5)
+# print(10>=5)
+# print(10<=5)
+# print(10>5 and 10<5)
+# print(10>5 or 10<5)
+# print(not(10>5))
+
+#Practice Set -1 
+# 01 - print("Hello, World! Welcome to Python.")
+# print("Twinkle, twinkle, little star,\nHow I wonder what you are!")
+
+# 02 - a = "Nevil"
+# age = 19
+# height = 5.7
+# is_student = True
+# print(a,age,height,is_student)
+
+# 03 - num="45"
+# b = (int(num)+10)
+# print(b)
+
+# 04 - food = input("Enter your favourite food : ")
+# print(f"Wow! I also like {food}")
+
+# 05 - print("Hello \"Python\" World!\nThis is on a new line.\nThis is a tab → \tafter tab")
+
+# 06 - num = int(input("Enter a number : "))
+# print(num**2)
+# print(num**3)

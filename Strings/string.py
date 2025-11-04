58da695c17fd88dffc9fe6f0042fcd1bf20bdc4b
nevil811 <nevilbarick69@gmail.com>
2025-11-04 11:53:59 +0530

    Strings and its Functions

---
+++ Strings/string.py
@@ -0,0 +1,23 @@
+# str1="Name"
+# print(str1)
+
+#String Slicing
+# name = "Nevil"
+# print(name[0::2])
+
+#String Functions
+# str='nevilsanatan'
+# print(len(str))
+# print(str.endswith("an"))
+# print(str.count("a"))
+# print(str.capitalize())
+# print(str.upper())
+# print(str.find("a"))
+# print(str.replace("nevil","raj"))
+
+
+#Escape characters
+# print("My name is \'Nevil\' \\ \'Raj\'")
+
+# name = "Nevil"
+# print(f"My name is {name}")

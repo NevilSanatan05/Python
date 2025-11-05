7d1ea7153a93d2244b5c067c38de772b05f8853b
nevil811 <nevilbarick69@gmail.com>
2025-11-05 13:12:35 +0530

    Lists

---
+++ Lists & Tuples/lists.py
@@ -0,0 +1,10 @@
+fruits=["Apple","Mango",1,True]
+print(fruits[0])
+
+numbers = [1,3,4,2,6]
+print(numbers.index(2))
+numbers.sort()
+print(numbers)
+print(numbers.count(4))
+numbers.append(10)
+print(numbers)

62960797c8d4ec22aef4678c8fb6f1af032f55f2
nevil811 <nevilbarick69@gmail.com>
2025-11-14 12:09:48 +0530

    Functions

---
+++ Functions & Recursions/functions.py
@@ -0,0 +1,17 @@
+# def greet(name):
+#   invitation = f"Hello, {name}! Welcome aboard."
+#   return invitation
+# print(greet("Alice"))
+
+# def greet(name):
+#   print(f"Hello, {name}! Welcome aboard.")
+# greet("Alice")
+
+
+# def sum(a,b):
+#   return a + b
+# print(sum(3,5))
+
+# def diff(a,b):
+#   print(a - b)
+# diff(3,4)
